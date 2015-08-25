e782fe28d9fe206d4159c1ce3759019466f5987e
Hiroshi Yamaguchi <1038223428@mail.ecc.u-tokyo.ac.jp>
2015-08-25 16:45:01 +0900

    richer wikify_full

--- rime/plugins/wikify_full.py
+++ rime/plugins/wikify_full.py
@@ -24,6 +24,7 @@
 
 import commands as builtin_commands
 import getpass
+import hashlib
 import itertools
 import os.path
 import re
@@ -63,6 +64,33 @@ def SafeUnicode(s):
     s = s.decode('utf-8')
   return s
 
+def GetFileSize(dir, filename):
+  filepath = os.path.join(dir, filename)
+  if os.path.exists(filepath):
+    return '%dB' % os.path.getsize(filepath)
+  else:
+    return '-'
+
+
+def GetFileHash(dir, filename):
+  filepath = os.path.join(dir, filename)
+  if os.path.exists(filepath):
+    f = open(filepath)
+    r = f.read()
+    f.close()
+    return hashlib.md5(SafeUnicode(r)).hexdigest()
+  else:
+    return ''
+
+def GetFileComment(dir, filename):
+  filepath = os.path.join(dir, filename)
+  if os.path.exists(filepath):
+    f = open(filepath)
+    r = f.read()
+    f.close()
+    return SafeUnicode(r).replace('\n', '&br;').replace('|', '&#x7c;')
+  else:
+    return ''
 
 class Project(targets.registry.Project):
   @taskgraph.task_method
@@ -70,27 +98,53 @@ class Project(targets.registry.Project):
     if not self.wikify_config_defined:
       ui.errors.Error(self, 'wikify_config() is not defined.')
       yield None
-    wiki = yield self._GenerateWikiFull(ui)
-    self._UploadWiki(wiki, ui)
+    wikiFull = yield self._GenerateWikiFull(ui)
+    self._UploadWiki(wikiFull, ui)
     yield None
 
   @taskgraph.task_method
   def _GenerateWikiFull(self, ui):
     yield self.Clean(ui) # 重すぎるときはコメントアウト
     # Get system information.
-    rev = builtin_commands.getoutput('svnversion')
+    rev = SafeUnicode(builtin_commands.getoutput('git show -s --oneline').replace('\n', ' ').replace('\r', ' '))
     username = getpass.getuser()
     hostname = socket.gethostname()
+
     # Generate content.
-    wiki = (u'#contents\n'
-            u'このセクションは wikify_full plugin により自動生成されています '
-            u'(rev.%(rev)s, uploaded by %(username)s @ %(hostname)s)\n' % 
-            {'rev': rev, 'username': username, 'hostname': hostname})
+    wiki =  u'** Summary\n'
+    wiki += u'|||CENTER:|CENTER:|CENTER:|CENTER:|CENTER:|c\n'
+    wiki += u'|~問題|~担当|~解答|~入力|~出力|~入検|~出検|\n'
+
+    wikiFull =  u'** Detail\n'
+
     results = yield taskgraph.TaskBranch([
         self._GenerateWikiFullOne(problem, ui)
         for problem in self.problems])
-    wiki += ''.join(results)
-    yield wiki
+    (wikiResults, wikiFullResults) = zip(*results)
+    wiki += ''.join(wikiResults)
+    wikiFull += ''.join(wikiFullResults)
+
+    environments =  '** Environments\n'
+    environments += ':gcc:|'   + builtin_commands.getoutput('gcc --version')  + '\n'
+    environments += ':g++:|'   + builtin_commands.getoutput('g++ --version')  + '\n'
+    environments += ':javac:|' + builtin_commands.getoutput('javac -version') + '\n'
+    environments += ':java:|'  + builtin_commands.getoutput('java -version')  + '\n'
+
+    errors = '** Error Messages\n'
+    if ui.errors.HasError():
+      errors += ':COLOR(red):ERROR:|\n'
+      for e in ui.errors.errors:
+        errors += '--' + e + '\n'
+    if ui.errors.HasWarning():
+      errors += ':COLOR(yellow):WARNING:|\n'
+      for e in ui.errors.warnings:
+        errors += '--' + e + '\n'
+    errors = SafeUnicode(errors)
+
+    yield u'#contents\n' + (u'このセクションは wikify_full plugin により自動生成されています '
+            u'(rev.%(rev)s, uploaded by %(username)s @ %(hostname)s)\n' %
+            {'rev': rev, 'username': username, 'hostname': hostname}
+            ) + wiki + environments + errors + wikiFull
 
   @taskgraph.task_method
   def _GenerateWikiFullOne(self, problem, ui):
@@ -98,19 +152,30 @@ class Project(targets.registry.Project):
 
     # Get status.
     title = SafeUnicode(problem.title) or 'No Title'
+    wiki_name = SafeUnicode(problem.wiki_name) or 'No Wiki Name'
+    assignees = problem.assignees
+    if isinstance(assignees, list):
+      assignees = ','.join(assignees)
+    assignees = SafeUnicode(assignees)
+
     # Get various information about the problem.
-    wiki = '***' + title + '\n'
+    wikiFull = '***' + title + '\n'
     solutions = sorted(problem.solutions, key=lambda x: x.name)
     solutionnames = [solution.name for solution in solutions]
-    wiki += '|~' + '|~'.join(['testcase'] + solutionnames) + '|h\n'
+    captions = [name.replace('-', ' ').replace('_', ' ') for name in solutionnames]
+    wikiFull += '|CENTER:~' + '|CENTER:~'.join(['testcase', 'in', 'diff', 'md5'] + captions + ['Comments']) + '|h\n'
+    formats = ['RIGHT:' for solution in solutions]
+    wikiFull += '|' + '|'.join(['LEFT:', 'RIGHT:', 'RIGHT:', 'LEFT:'] + formats + ['LEFT:']) + '|c\n'
 
     dics = {}
     for testcase in problem.testset.ListTestCases():
       testname = os.path.splitext(os.path.basename(testcase.infile))[0]
       dics[testname] = {}
+    results = []
     for solution in solutions:
       name = solution.name
       test_result = (yield problem.testset.TestSolution(solution, ui))[0]
+      results.append(test_result)
       for (testcase, result) in test_result.results.items():
         testname = os.path.splitext(os.path.basename(testcase.infile))[0]
         dics.setdefault(testname, {})[name] = (result.verdict, result.time)
@@ -122,14 +187,84 @@ class Project(targets.registry.Project):
         cols.append(dics[testname].get(name, (test.TestCaseResult.NA, None)))
       lists.append((testname, cols))
     rows = []
+    dir = problem.testset.out_dir
     for casename, cols in lists:
       rows.append(
           '|' +
-          '|'.join([casename] + [self._GetMessage(*t) for t in cols]) +
+          '|'.join(
+            [
+              casename.replace('_', ' ').replace('-', ' '),
+              GetFileSize(dir, casename + consts.IN_EXT),
+              GetFileSize(dir, casename + consts.DIFF_EXT),
+              GetFileHash(dir, casename + consts.IN_EXT)
+            ]
+            + [self._GetMessage(*t) for t in cols]
+            + [GetFileComment(dir, casename + '.comment')]
+            ) +
           '|\n')
-    wiki += ''.join(rows)
+    wikiFull += ''.join(rows)
 
-    yield wiki
+    # Fetch test results.
+    #results = yield problem.Test(ui)
+
+    # Get various information about the problem.
+    num_solutions = len(results)
+    num_tests = len(problem.testset.ListTestCases())
+    correct_solution_results = [result for result in results
+                                if result.solution.IsCorrect()]
+    num_corrects = len(correct_solution_results)
+    num_incorrects = num_solutions - num_corrects
+    num_agreed = len([result for result in correct_solution_results
+                      if result.expected])
+    need_custom_judge = problem.need_custom_judge
+
+    # Solutions:
+    if num_corrects >= 2:
+      cell_solutions = BGCOLOR_GOOD
+    elif num_corrects >= 1:
+      cell_solutions = BGCOLOR_NOTBAD
+    else:
+      cell_solutions = BGCOLOR_BAD
+    cell_solutions += '%d+%d' % (num_corrects, num_incorrects)
+
+    # Input:
+    if num_tests >= 20:
+      cell_input = BGCOLOR_GOOD + str(num_tests)
+    else:
+      cell_input = BGCOLOR_BAD + str(num_tests)
+
+    # Output:
+    if num_corrects >= 2 and num_agreed == num_corrects:
+      cell_output = BGCOLOR_GOOD
+    elif num_agreed >= 2:
+      cell_output = BGCOLOR_NOTBAD
+    else:
+      cell_output = BGCOLOR_BAD
+    cell_output += '%d/%d' % (num_agreed, num_corrects)
+
+    # Validator:
+    if problem.testset.validators:
+      cell_validator = CELL_GOOD
+    else:
+      cell_validator = CELL_BAD
+
+    # Judge:
+    if need_custom_judge:
+      custom_judges = [judge for judge in problem.testset.judges
+                       if judge.__class__ != basic_codes.InternalDiffCode]
+      if custom_judges:
+        cell_judge = CELL_GOOD
+      else:
+        cell_judge = CELL_BAD
+    else:
+      cell_judge = CELL_NA
+
+    # Done.
+    wiki = (('|[[%(title)s>%(wiki_name)s]]|%(assignees)s|'
+            '%(cell_solutions)s|%(cell_input)s|%(cell_output)s|'
+            '%(cell_validator)s|%(cell_judge)s|\n') % locals())
+
+    yield (wiki, wikiFull)
 
   def _GetMessage(self, verdict, time):
     if verdict is test.TestCaseResult.NA:
