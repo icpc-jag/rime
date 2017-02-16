e87a3aa2d206f62cfe39a9b7ab5b37d9c309db97
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-02-16 20:51:25 +0900

    Clean up setup.py.
    - Replace rime-plus with rime.
    - Bump the version to 2.0.dev1, hoping we can release v2.0 at some
      point.
    - Avoid using setuptools.find_packages() which incorrectly
      includes other directories like nose and tests.
    - Cosmetic message changes.

--- setup.py
+++ setup.py
@@ -1,17 +1,39 @@
-from setuptools import setup, find_packages
+# Copyright (c) 2017 Rime Project.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 import sys
 
+import setuptools
 
-if sys.version_info[0] == 2:
-  setup(
-    name = "rime_plus",
-    version = "1.0.0",
-    scripts          = ['bin/rime', 'bin/rime_init'],
-    packages         = find_packages(),
-    package_dir      = {'rime': 'rime'},
-  )
-else:
-  print("rime-plus support only python2.")
-  print("please use:")
-  print("    python2 -m pip install git+https://github.com/icpc-jag/rime-plus")
+
+if sys.version_info[0] != 2:
+  print('Rime supports Python 2 only at this moment.')
+  print('Please use:')
+  print('    python2 -m pip install git+https://github.com/icpc-jag/rime')
   sys.exit(1)
+
+setuptools.setup(
+  name='rime',
+  version='2.0.dev1',
+  scripts=['bin/rime', 'bin/rime_init'],
+  packages=['rime', 'rime.basic', 'rime.core', 'rime.plugins', 'rime.util'],
+  package_dir={'rime': 'rime'},
+)
