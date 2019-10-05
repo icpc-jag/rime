ab78ac3aad2d53fda96ddb71b840f278af08ef41
blue-jam <blue-jam@users.noreply.github.com>
2019-10-05 14:27:38 +0900

    Add unit tests for htmlify_full #58

---
+++ tests/plugins_test/test_htmlify_full.py
@@ -0,0 +1,36 @@
+import unittest
+
+import mock
+from rime.util import struct
+
+from rime.plugins import htmlify_full
+
+
+class TestHtmlifyProject(unittest.TestCase):
+    def test_do_clean(self):
+        ui = mock.MagicMock()
+        ui.options = struct.Struct({'skip_clean': False})
+        project = htmlify_full.Project('project', 'base_dir', None)
+        project.problems = []
+        project.Clean = mock.MagicMock()
+
+        task_graph = project._GenerateHtmlFull(ui)
+        task_graph.Continue()
+
+        project.Clean.called_once_with(ui)
+
+    def test_skip_clean(self):
+        ui = mock.MagicMock()
+        ui.options = struct.Struct({'skip_clean': True})
+        project = htmlify_full.Project('project', 'base_dir', None)
+        project.problems = []
+        project.Clean = mock.MagicMock()
+
+        task_graph = project._GenerateHtmlFull(ui)
+        task_graph.Continue()
+
+        project.Clean.assert_not_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
