ceecfa9dcb4174b2d23067e1b6660e3b158244bf
Naoto Mizuno <naotomizuno@preferred.jp>
2020-10-15 10:01:39 +0900

    Fix for Python 2.7

--- rime/core/taskgraph.py
+++ rime/core/taskgraph.py
@@ -401,7 +401,10 @@ class FiberTaskGraph(object):
     def Run(self, init_task):
         assert not self.running
         self.running = True
-        self.first_tick = time.perf_counter()
+        if sys.version_info.major == 2:
+            self.first_tick = time.clock()
+        else:
+            self.first_tick = time.perf_counter()
         self.last_tick = self.first_tick
         self.cumulative_parallelism = 0.0
         self._BranchTask(None, [init_task])
@@ -749,7 +752,10 @@ class FiberTaskGraph(object):
             self._InterruptTask(subtask)
 
     def _UpdateCumulativeParallelism(self):
-        cur_tick = time.perf_counter()
+        if sys.version_info.major == 2:
+            cur_tick = time.clock()
+        else:
+            cur_tick = time.perf_counter()
         self.cumulative_parallelism += (
             (cur_tick - self.last_tick) * len(self.blocked_tasks))
         self.last_tick = cur_tick
